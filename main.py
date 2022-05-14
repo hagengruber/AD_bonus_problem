f915b428bb3f7ed60beda1fd1cfae870a8f196fc
Florian Hagengruber <github@hagengruber.dev>
2022-05-14 21:19:29 +0200

    Creates a csv File for statistics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from IPython import get_ipython
+import sys
+from tqdm import tqdm
 
 #
 # ACHTUNG: Script muss mit ipython aufgerufen werden
@@ -6,9 +8,25 @@ from IPython import get_ipython
 
 ipython = get_ipython()
 
+
+class Logger:
+    stdout = sys.stdout
+    messages = []
+
+    def start(self):
+        sys.stdout = self
+
+    def stop(self):
+        sys.stdout = self.stdout
+
+    def write(self, text):
+        self.messages.append(text)
+
+
 f = open('file.txt')
 bibel_text = f.read()
 bibel = ""
+pattern = "Moin"
 
 
 def find_brute(t, p):
@@ -65,7 +83,7 @@ def find_kmp(t, p):
             j += 1
             k += 1
         elif k > 0:
-            k = fail[k-1]
+            k = fail[k - 1]
         else:
             j += 1
 
@@ -73,7 +91,6 @@ def find_kmp(t, p):
 
 
 def compute_kmp_fail(p):
-
     m = len(p)
     fail = [0] * m
     j = 1
@@ -85,7 +102,7 @@ def compute_kmp_fail(p):
             j += 1
             k += 1
         elif k > 0:
-            k = fail[k-1]
+            k = fail[k - 1]
         else:
             j += 1
 
@@ -94,37 +111,127 @@ def compute_kmp_fail(p):
 
 def do_brute_force(p):
     if p == 0:
-        find_brute(bibel, 'Moin')
+        find_brute(bibel, pattern)
     else:
-        print(find_brute(bibel, 'Moin'))
+        print(find_brute(bibel, pattern))
 
 
 def do_boyer_moore(p):
     if p == 0:
-        find_boyer_moore(bibel, 'Moin')
+        print(find_boyer_moore(bibel, pattern))
     else:
-        print(find_boyer_moore(bibel, 'Moin'))
+        print(find_boyer_moore(bibel, pattern))
 
 
 def do_kmp(p):
     if p == 0:
-        find_kmp(bibel, 'Moin')
+        find_kmp(bibel, pattern)
     else:
-        print(find_kmp(bibel, 'Moin'))
+        print(find_kmp(bibel, pattern))
 
 
-for a in range(5, 100):
+zeichen = ""
+al1 = "Brute Force"
+al2 = "Boyer Moore"
+al3 = "KMP"
 
-    bibel = bibel_text[:a] + "Moin"
-    print(str(len(bibel)))
+log = Logger()
 
-    # print("Länge " + str(i))
-    # print(bibel)
-    print("Brute Force: ", end='')
-    ipython.run_line_magic("timeit",  "do_brute_force(0)")
+start = 5
+end = len(bibel_text)
+print((end - start) / 191)
+sprung = int((end - start) / 191)
+output = tqdm(total=int((end - start) / sprung))
 
-    print("Boyer Moore: ", end='')
-    ipython.run_line_magic("timeit",  "do_boyer_moore(0)")
+for a in range(start, end, int((end - start) / 191)):
 
-    print("KMP: ", end='')
-    ipython.run_line_magic("timeit",  "do_kmp(0)")
+    # os.system("cls")
+    # print("Pattern: " + pattern)
+    bibel = bibel_text[:a] + pattern
+
+    # bibel = bibel_text + "Moin"
+
+    zeichen += ";" + str(len(bibel)) + " Zeichen"
+
+    fz = open("fz.txt", "w")
+    fz.write(zeichen)
+    fz.close()
+
+    log.start()
+    ipython.run_line_magic("timeit", "do_brute_force(0)")
+    log.stop()
+
+    m = log.messages
+    time = float(log.messages[len(m) - 2].split(' ')[0])
+    type = log.messages[len(m) - 2].split(' ')[1]
+
+    # print("Eigentliche Zeit: " + str(time) + " " + type)
+
+    if type == "us":
+        time /= 1000
+    elif type == "s":
+        time *= 1000
+
+    # print("Konvertierte Zeit: " + str(time) + " ms")
+
+    al1 += ";" + str(time).replace('.', ',')
+
+    log.start()
+    ipython.run_line_magic("timeit", "do_boyer_moore(0)")
+    log.stop()
+
+    m = log.messages
+    time = float(log.messages[len(m) - 2].split(' ')[0])
+    type = log.messages[len(m) - 2].split(' ')[1]
+
+    # print("Eigentliche Zeit: " + str(time) + " " + type)
+
+    if type == "us":
+        time /= 1000
+    elif type == "s":
+        time *= 1000
+
+    # print("Konvertierte Zeit: " + str(time) + " ms")
+
+    al2 += ";" + str(time).replace('.', ',')
+
+    log.start()
+    ipython.run_line_magic("timeit", "do_kmp(0)")
+    log.stop()
+
+    m = log.messages
+    time = float(log.messages[len(m) - 2].split(' ')[0])
+    type = log.messages[len(m) - 2].split(' ')[1]
+
+    # print("Eigentliche Zeit: " + str(time) + " " + type)
+
+    if type == "us":
+        time /= 1000
+    elif type == "s":
+        time *= 1000
+
+    # print("Konvertierte Zeit: " + str(time) + " ms")
+
+    al3 += ";" + str(time).replace('.', ',')
+
+    f1 = open("a1.txt", "w")
+    f1.write(al1)
+    f1.close()
+
+    f2 = open("a2.txt", "w")
+    f2.write(al2)
+    f2.close()
+
+    f3 = open("a3.txt", "w")
+    f3.write(al3)
+    f3.close()
+
+    output.update()
+
+f = open("test.csv", "w")
+f.write(zeichen + "\n")
+f.write(al1 + "\n")
+f.write(al2 + "\n")
+f.write(al3 + "\n")
+
+f.close()
